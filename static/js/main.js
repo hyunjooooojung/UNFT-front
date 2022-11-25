dc8cf747b98887c064b90af7d764c271df30cb90
LeeHyunji <hyunji015@gmail.com>
2022-11-26 02:16:47 +0900

    Remove: 불필요한 console.log, 주석 삭제

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,6 @@
 // profile tab jquery
 $('.tab_item').on("click",function(e){
     e.preventDefault(); 
-    console.log(this);
-    console.log($(this).find("a").attr("href"));
     const target_content_id = $(this).find("a").attr("href");
     $(".tab_item").removeClass('active');
     $(this).addClass("active");
