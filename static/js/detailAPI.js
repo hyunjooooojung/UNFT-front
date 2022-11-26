e2311c4de53cf79f28aed0290566d88534c45685
haeminchoi2 <sjhk02@gmail.com>
2022-11-26 14:21:19 +0900

    Feat: U-NFT 상세페이지 거래내역 내용 수정 #41
    - 거래내역이 없을 때, 제안 내역 개수에 상관없이 '거래 내역이 없습니다.' 한번만 출력

--- static/js/detailAPI.js
+++ static/js/detailAPI.js
@@ -141,6 +141,7 @@ async function handleDealDetail(){
     }).then(result => {
         let deal_body = document.getElementById("deal_body")
         deal_body.innerHTML="";
+        let deal_count = 0;
         if ("message" in result){
             null_item = document.createElement("div");
             null_item.className = "tr"
@@ -173,21 +174,22 @@ async function handleDealDetail(){
                         // UNFT 마지막 거래가 삽입
                         let last_price_div = document.querySelector(".unft_card_price")
                         last_price_div.querySelector(".price").innerText = `${result[0]["price"]}`
-
-                }else{
-                    null_item = document.createElement("div");
-                    null_item.className = "tr"
-                    null_item.innerHTML = `
-                                        <div class="td">
-                                            <span>거래 내역이 없습니다.</span>
-                                        </div>
-                                        `
-                    deal_body.append(null_item)
+                        deal_count += 1
                 }
             });
+            if (deal_count === 0){
+            null_item = document.createElement("div");
+            null_item.className = "tr";
+            null_item.innerHTML = `
+                                <div class="td">
+                                    <span>거래 내역이 없습니다.</span>
+                                </div>
+                                `;
+            deal_body.append(null_item);
+            };
         };
     }).catch(error => {
-        console.error(error.message)
+        console.error(error.message);
     });
 };
 
